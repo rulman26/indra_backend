93e5961d5362b15049a222a03edbb64221b8e199
rulman26 <rulman26@gmail.com>
2022-04-04 11:52:57 -0500

    datos de pruebas

---
+++ functions/tests/app.spec.js
@@ -0,0 +1,29 @@
+var request = require('supertest')
+describe('obtener datos persona', () => {
+    test('obtener datos swapi', async () => {
+        const response = await request('https://nb5ghd88r3.execute-api.us-east-1.amazonaws.com/dev/').get('obtener-persona-por-id?id=1').send();
+        expect(response.status).toBe(200)
+    })
+
+    test('paginar personas', async () => {
+        const response = await request('https://nb5ghd88r3.execute-api.us-east-1.amazonaws.com/dev/').get('paginar-personas?pagina=0&limite=10').send();
+        expect(response.status).toBe(200)
+    })
+    
+    test('guardar persona', async () => {
+        const response = await request('https://nb5ghd88r3.execute-api.us-east-1.amazonaws.com/dev/').post('guardar-persona')
+        .send({
+            "nombre":"Maria 2322",
+            "altura":"170",
+            "masa":"73",
+            "colorPelo":"marron",
+            "colorPiel":"blanca",
+            "colorOjo":"negro",
+            "fechaNacimiento":"27/09/1992",
+            "genero":"F"
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/);
+        expect(response.status).toBe(200)
+    })
+}) 
